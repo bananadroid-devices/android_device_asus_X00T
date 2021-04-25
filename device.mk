1009c7f000b55d98a52e2a0b948a1546900404e5
Saurabh Charde <saurabhchardereal@gmail.com>
2021-04-25 11:55:49 +0530

    X00T: Add uhid, context_hub to sensor service
    * from upstream
    * also move to device rc since overriding from device makefile gives warning
    
    Signed-off-by: Saurabh Charde <saurabhchardereal@gmail.com>

--- device.mk
+++ device.mk
@@ -323,8 +323,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
-    $(LOCAL_PATH)/configs/sensors/android.hardware.sensors@1.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.sensors@1.0-service.rc
+    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
 # Shims
 PRODUCT_PACKAGES += \
