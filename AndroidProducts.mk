34cd067c9ebe42f24259f979d90e8c63a21a28e0
Rizky Benggolo <travarilo@gmail.com>
2021-07-19 15:24:07 +0700

    X00T: Welcome BananaDroid
    Change-Id: If11ea120012f49361a33d914ab21868422ad1a53

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_X00T.mk
+    $(LOCAL_DIR)/banana_X00T.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_X00T-user \
-    arrow_X00T-userdebug \
-    arrow_X00T-eng
+    banana_X00T-user \
+    banana_X00T-userdebug \
+    banana_X00T-eng
