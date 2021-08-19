KudProject/device_asus_X00T

X00T: add touch hal hidl for gestures
Edits:
 - adapt code to work with single node
 - add all supported gestures
 - add custom gesture strings
 - set custom default actions for each gesture

Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
Signed-off-by: Albert I <kras@raphielgang.org>

touch/service.cpp
@@ -1,0 +1,46 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "vendor.lineage.touch@1.0-service.X00TD"
+
+#include <android-base/logging.h>
+#include <hidl/HidlTransportSupport.h>
+
+#include "TouchscreenGesture.h"
+
+using ::android::OK;
+using ::android::sp;
+
+using ::vendor::lineage::touch::V1_0::ITouchscreenGesture;
+using ::vendor::lineage::touch::V1_0::implementation::TouchscreenGesture;
+
+int main() {
+    sp<ITouchscreenGesture> gestureService = new TouchscreenGesture();
+
+    android::hardware::configureRpcThreadpool(1, true /*callerWillJoin*/);
+
+    if (gestureService->registerAsService() != OK) {
+        LOG(ERROR) << "Cannot register touchscreen gesture HAL service.";
+        return 1;
+    }
+
+    LOG(INFO) << "Touchscreen HAL service ready.";
+
+    android::hardware::joinRpcThreadpool();
+
+    LOG(ERROR) << "Touchscreen HAL service failed to join thread pool.";
+    return 1;
+}

bananadroid-devices/android_device_asus_X00T

 - add manifest entry as vintf fragment
 - add vendor prefix
 - adapt to TouchGestures
Signed-off-by: Rizky Benggolo <travarilo@gmail.com>
Change-Id: I8b0b656eb0356be96e72f3a5c1935dae9853a2e7

touch/service.cpp
@@ -1,0 +1,46 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "vendor.lineage.touch@1.0-service.X00T"
+
+#include <android-base/logging.h>
+#include <hidl/HidlTransportSupport.h>
+
+#include "TouchscreenGesture.h"
+
+using ::android::OK;
+using ::android::sp;
+
+using ::vendor::lineage::touch::V1_0::ITouchscreenGesture;
+using ::vendor::lineage::touch::V1_0::implementation::TouchscreenGesture;
+
+int main() {
+    sp<ITouchscreenGesture> gestureService = new TouchscreenGesture();
+
+    android::hardware::configureRpcThreadpool(1, true /*callerWillJoin*/);
+
+    if (gestureService->registerAsService() != OK) {
+        LOG(ERROR) << "Cannot register touchscreen gesture HAL service.";
+        return 1;
+    }
+
+    LOG(INFO) << "Touchscreen HAL service ready.";
+
+    android::hardware::joinRpcThreadpool();
+
+    LOG(ERROR) << "Touchscreen HAL service failed to join thread pool.";
+    return 1;
+}