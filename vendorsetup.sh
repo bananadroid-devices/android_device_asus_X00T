9fcd2e6c3fbebb2e98ef773c304c8fcafe54784b
Rizky Benggolo <travarilo@gmail.com>
2021-08-19 19:24:40 +0700

    X00T: Clone HALs from AOSP-11
    Change-Id: I4641fdcb3ea94fd05841ce4f75210f5a811e7b8e

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,11 @@
 export SKIP_ABI_CHECKS=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TEMPORARY_DISABLE_PATH_RESTRICTIONS=true
+
+rm -rf hardware/qcom-caf/msm8998/display
+rm -rf hardware/qcom-caf/msm8998/media
+rm -rf hardware/qcom-caf/msm8998/audio
+
+git clone -b 11-caf-msm8998 https://github.com/bananadroid/android_hardware_qcom_display.git hardware/qcom-caf/msm8998/display
+git clone -b 11-caf-msm8998 https://github.com/bananadroid/android_hardware_qcom_media.git hardware/qcom-caf/msm8998/media
+git clone -b 11-caf-msm8998 https://github.com/bananadroid/android_hardware_qcom_audio.git hardware/qcom-caf/msm8998/audio
